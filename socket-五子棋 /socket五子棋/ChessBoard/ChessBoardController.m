431517783026eddb779b32dd096b5e64682ec268
xiaoshi <sjl_leaf@163.com>
2016-05-26 17:44:53 +0800

    Chessboard add game over Button

--- socket-五子棋 /socket五子棋/ChessBoard/ChessBoardController.m
+++ socket-五子棋 /socket五子棋/ChessBoard/ChessBoardController.m
@@ -8,7 +8,26 @@
 
 #import "ChessBoardController.h"
 
+#define appWidth    [UIScreen mainScreen].bounds.size.width
+#define appHeight   [UIScreen mainScreen].bounds.size.height
+
 @interface ChessBoardController ()
+
+#pragma mark - 界面
+@property (nonatomic, strong) UIButton *overButton;
+
+#pragma mark - 游戏数据
+/**
+ *  当前下棋的个数
+ */
+@property (nonatomic, strong) UILabel *countLabel;
+
+/**
+ *  是否轮到自己
+ */
+@property (nonatomic, assign) BOOL isMe;
+
+
 
 @end
 
@@ -17,6 +36,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //self.automaticallyAdjustsScrollViewInsets = NO;
+    self.navigationController.navigationBarHidden = YES;
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.overButton];
+    self.overButton.frame = CGRectMake(appWidth - 100, 25, 80, 40);
+    
+    [self setOverButtonLayer];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +50,39 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - 私有方法
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/**
+ *  设置按钮的渲染
+ */
+- (void)setOverButtonLayer
+{
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:self.overButton.bounds byRoundingCorners:(UIRectCornerAllCorners) cornerRadii:CGSizeMake(10, 5)];
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.frame = self.overButton.bounds;
+    maskLayer.path = path.CGPath;
+    self.overButton.layer.mask = maskLayer;
 }
-*/
+/**
+ *  结束游戏
+ */
+- (void)clickOverGame
+{
+    NSLog(@"结束游戏");
+}
+
+#pragma mark - 懒加载
+- (UIButton *)overButton
+{
+    if (!_overButton) {
+        _overButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _overButton.backgroundColor = [UIColor redColor];
+        [_overButton setTitle:@"结束游戏" forState:UIControlStateNormal];
+        [_overButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        _overButton.titleLabel.font = [UIFont systemFontOfSize:18];
+        [_overButton addTarget:self action:@selector(clickOverGame) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _overButton;
+}
 
 @end
